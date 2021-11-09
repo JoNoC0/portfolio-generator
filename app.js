64d3345b04c4aacdb89946b45eddf348edf9276c
JoNoC0 <jnguyeningco@gmail.com>
2021-11-08 23:39:49 -0600

    inquirer prompt

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
+const inquirer = require("inquirer");
 
-const fs = require('fs');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const generatePage = require('./src/page-template');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-const pageHTML = generatePage(name, github);
+// const pageHTML = generatePage(name, github);
 // capture user input
 // const printProfileData = profileDataArr => {
 //     for (let i = 0; i < profileDataArr.length; i += 1) {
@@ -20,9 +16,18 @@ const pageHTML = generatePage(name, github);
 
 // printProfileData(profileDataArgs);
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
+inquirer
+  .prompt ([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
